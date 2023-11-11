48ca2e09e4b8c008c13c3f589f805eba459bbde6
Sbosaty <115923061+Sbosaty@users.noreply.github.com>
2023-11-11 20:54:42 +0200

    Update user .js

--- models /user .js
+++ models /user .js
@@ -1,30 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-// Load User model
+
 const User = require('../models/user');
 
-// @route POST api/users/register
-// @desc Register user
-// @access Public
 router.post('/register', (req, res) => {
   // Your registration logic here
 });
 
-// @route POST api/users/login
-// @desc Login user and return JWT token
-// @access Public
+
 router.post('/login', (req, res) => {
-  // Your login logic here
+  
 });
 
-// @route POST api/users/update
-// @desc Update user profile
-// @access Private
+
 router.post('/update', (req, res) => {
-  // Your update logic here, make sure to authenticate
+
 });
 
-// Add routes for password change and profile picture upload
+
 
 module.exports = router;
