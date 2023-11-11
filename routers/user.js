9352ccb6e7dd7e2fa0698384ae136e0ef0120458
Sbosaty <115923061+Sbosaty@users.noreply.github.com>
2023-11-11 21:36:43 +0200

    Create user.js

---
+++ routers/user.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+
+const User = require('../models/user');
+
+
+router.post('/register', (req, res) => {
+ 
+});
+
+
+router.post('/login', (req, res) => {
+  
+});
+
+
+router.post('/update', (req, res) => {
+
+});
+
+// Add routes for password and profile picture upload/////
+
+module.exports = router;
