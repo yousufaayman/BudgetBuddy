9af8d1d5e80e80f69b253aa87c54ddbd2a20c4c3
yousufaayman <yousufaymanh04@gmail.com>
2023-11-22 20:51:20 +0200

    adjusted file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import {ContainerStyled} from './Components/Styles/Container.Styled'
-import {RegestrationForm} from './Components/RegestrationForm'
-import { RegNavBar } from './Components/RegNavbar';
-import {GridItem} from './Components/Styles/GridItem'
+import {RegestrationPage} from './Components/RegestrationPage'
 
 function App() {
   return (
-    <ContainerStyled display="grid" gridrows="1fr 4fr" gridcolumns="2fr 4fr 2fr">
-        <GridItem gridarea = "1 / 1 / 1 / 4" qgridarea = "1 / 1 / 1 /4"><RegNavBar/></GridItem>
-        <GridItem gridarea = "2 / 2 / 3 / 3" qgridarea = " 2 / 1 / 4 / 4"><RegestrationForm /></GridItem>
-    </ContainerStyled>
+    <RegestrationPage/>
   );
 }
 
