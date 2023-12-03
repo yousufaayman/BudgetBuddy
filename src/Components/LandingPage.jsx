901a98bcc2345e517b97d6f0b15042bf6d49f27d
yousufaayman <yousufaymanh04@gmail.com>
2023-12-04 00:02:53 +0200

    Adjusted Responsivness

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -7,9 +7,9 @@ import './Styles/LandingPage.css'
 
 export const LandingPage = () =>  {
   return (
-    <ContainerStyled className="page" display = "grid" gridColumns= "4fr 8fr 1fr" gridRows= "2fr 4fr 2fr" QLgridRows="1fr 5fr 1fr" QLgridColumns="1fr 3fr 2fr 1fr" QMgridRows="1fr 5fr 1fr" QMgridColumns="1fr 2fr 2f">
-        <GridItem gridArea = "2 / 1 / 3 / 2" QgridArea = "3 / 1 / 4 / 4"><LoginForm /></GridItem>
+    <ContainerStyled className="page" width="100%" height="100%" display = "grid" gridColumns= "4fr 8fr 1fr" gridRows= "2fr 6fr" QLgridRows="1fr 5fr 1fr" QLgridColumns="1fr 3fr 2fr 1fr" QMgridRows="1fr 5fr 1fr" QMgridColumns="1fr 2fr 2f">
         <GridItem gridArea = "1 / 1 / 2 / 4" QgridArea = " 1 / 1 / 2 / 4"><LandingTopBar/></GridItem>
+        <GridItem gridArea = "2 / 1 / 3 / 2" QgridArea = "3 / 1 / 4 / 4"><LoginForm /></GridItem>
         <GridItem gridArea = "2 / 2 / 3 / 4" QgridArea = " 2 / 1 / 3 / 4"><LandingContent/></GridItem>
     </ContainerStyled>
   );
