b26f6d046b293f06e320faf5402c74043835ba16
yousufaayman <yousufaymanh04@gmail.com>
2023-12-06 17:02:07 +0200

    Create Popup to allow user to change their current working wallet

---
+++ src/Components/WalletPopup.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useContext } from 'react';
+import './Styles/popups.css';
+import { GrClose } from 'react-icons/gr';
+import UserContext from './UserContext';
+import {WalletsDropwdoown} from './UserWalletsDropdown'
+
+export const WalletPopup = ({ isOpen, onClose, onChangeWallet}) => {
+  const {setWalletId} = useContext(UserContext);
+  const [selectedWallet, setselectedWallet] = useState('');
+
+
+  const handleSelectedWallet = (selectedWallet) => {
+    setselectedWallet(selectedWallet);
+    setWalletId(selectedWallet)
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div className="wallet-popup">
+
+            <button id="walletclose-btn" onClick={onClose}>
+              <GrClose style={{ color: '#7b0dcf' }} />
+            </button>
+
+            <WalletsDropwdoown onCategoryChange={handleSelectedWallet}/>
+            
+            <button id="walletclose-btn" onClick={onChangeWallet}>
+              Change Wallet
+            </button>
+
+        </div>
+      )}
+    </>
+  );
+};
+
+export default WalletPopup;
