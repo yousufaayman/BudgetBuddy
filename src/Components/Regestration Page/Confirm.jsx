c054f236e9aff5ea331e910b76e8ed3e549f9054
yousufaayman <yousufaymanh04@gmail.com>
2023-12-08 00:41:35 +0200

    Update confirm.jsx

--- src/Components/Regestration Page/Confirm.jsx
+++ src/Components/Regestration Page/Confirm.jsx
@@ -14,23 +14,23 @@ export const Confirm = ({ prevStep, values, handleSignUp }) => {
       <h1 className="form-title">Please review your info :)</h1>
 
       <LabelStyled gridcolumn="1 / 3" gridrow="3 / 4">
-        {"First Name:"} {values.firstName}
+        First Name:{values.firstName}
       </LabelStyled>
       <LabelStyled gridcolumn="1 / 3" gridrow="4 / 5">
-        {"Last Name:"} {values.lastName}
+        Last Name: {values.lastName}
       </LabelStyled>
       <label className="email">Email: {values.email}</label>
       <LabelStyled gridcolumn="1 / 3" gridrow="6 / 7">
-        {"Password:"} {values.password}
+        Password: {values.password}
       </LabelStyled>
       <LabelStyled gridcolumn="1 / 3" gridrow="7 / 8">
-        {"Currency:"} {values.currency}
+        Currency: {values.currency}
       </LabelStyled>
       <LabelStyled gridcolumn="1 / 3" gridrow="8 / 9">
-        {"Country:"} {values.country}
+        Country: {values.country}
       </LabelStyled>
       <LabelStyled gridcolumn="1 / 3" gridrow="9 / 10">
-        {"Monthly Income:"} {values.avgIncome}
+        Monthly Income: {values.avgIncome}
       </LabelStyled>
 
       <ButtonStyled
@@ -45,7 +45,7 @@ export const Confirm = ({ prevStep, values, handleSignUp }) => {
         gridarea="12 / 2 / 13 / 3"
         onClick={handleSignUp}
       >
-        {"Confirm :)"}
+        Confirm :)
       </ButtonStyled>
 
       <ButtonStyled
@@ -60,7 +60,7 @@ export const Confirm = ({ prevStep, values, handleSignUp }) => {
         gridarea="12 / 1 / 13 / 2"
         onClick={Previous}
       >
-        {"Previous"}
+        Previous
       </ButtonStyled>
     </div>
   );
