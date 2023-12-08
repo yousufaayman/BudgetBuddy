10d38eb57111c890e73771691fe391842c777e4f
yousufaayman <yousufaymanh04@gmail.com>
2023-12-08 07:52:20 +0200

    Revert "Merge branch 'Sign-In-(2)' into Routing-Application"
    This reverts commit 56c6991de10e6b8bb98389bcf7d68fe257a31a83, reversing
    changes made to 0a0761b0007dd08ef3468e35872dc78caf48d938.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import firebase from 'src/firebaseConfig';
-import './global.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
