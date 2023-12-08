e5c34a1799fa13c3c1784c136e770ddad2622b1d
yousufaayman <yousufaymanh04@gmail.com>
2023-12-08 22:06:29 +0200

    Update mailer.js

--- src/Services/mailer.js
+++ src/Services/mailer.js
@@ -1,1 +1,21 @@
+const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+    port: '465',
+    host: 'smtp.gmail.com',
+    secure: true,
+    auth: {
+      user: 'seif.elbosaty3@gmail.com',
+      pass: '',
+    },
+    secure: true,
+  })
+
+exports.sendMail = async(mailData) => {
+    transporter.sendMail(mailData, function (err, info) {
+        if(err)
+          console.log(err)
+        else
+          console.log(info)
+    })
+}
