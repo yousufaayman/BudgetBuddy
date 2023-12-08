c2376b21d5652429dee5ffdc4f761ab04e1d44b4
yousufaayman <yousufaymanh04@gmail.com>
2023-12-08 09:05:08 +0200

    merged server files

--- src/Services/server.js
+++ src/Services/server.js
@@ -429,6 +429,80 @@ app.get("/user/wallets/:userID", async (req, res) => {
   }
 });
 
+
+app.get('/user/getTotalIncome/:userID/:walletID', async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const walletID = req.params.walletID;
+
+    const userTransactionsRef = admin
+      .firestore()
+      .collection("users")
+      .doc(userID)
+      .collection("user_wallets")
+      .doc(walletID)
+      .collection("user_transactions");
+    const snapshot = await userTransactionsRef.get();
+    console.log(snapshot)
+
+    // Calculate total income
+    let totalIncome = 0;
+    snapshot.forEach((doc) => {
+      const transaction = doc.data();
+      if (transaction.type === 'income') {
+        totalIncome += parseFloat(transaction.amount);
+      }
+    });
+
+    // Round the total income to two decimal places
+    totalIncome = parseFloat(totalIncome.toFixed(2));
+    console.log(totalIncome)
+
+    res.status(200).json({
+      totalIncome,
+    });
+  } catch (error) {
+    console.error('Error retrieving total income:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/user/getTotalExpense/:userID/:walletID', async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const walletID = req.params.walletID;
+
+    const userTransactionsRef = admin
+      .firestore()
+      .collection("users")
+      .doc(userID)
+      .collection("user_wallets")
+      .doc(walletID)
+      .collection("user_transactions");
+    const snapshot = await userTransactionsRef.get();
+    console.log(snapshot)
+
+    // Calculate total expense
+    let totalExpense = 0;
+    snapshot.forEach((doc) => {
+      const transaction = doc.data();
+      if (transaction.type === 'expense') {
+        totalExpense += parseFloat(transaction.amount);
+      }
+    });
+
+    // Round the total expense to two decimal places
+    totalExpense = parseFloat(totalExpense.toFixed(2));
+
+    res.status(200).json({
+      totalExpense,
+    });
+  } catch (error) {
+    console.error('Error retrieving total expense:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // Start the server
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
