22eeb6b47697ee5ab637618b6c2e53d8de1b2319
Sbosaty <115923061+Sbosaty@users.noreply.github.com>
2023-12-08 14:27:03 +0200

    Update notification server.js

--- src/Services/notification server.js
+++ src/Services/notification server.js
@@ -1,15 +1,1 @@
-const { sendMail } = require('./Mailer')
-await sendMail({
-    from: 'seif.elbosaty3@gmail.com',
-    to: 'seif.elbosaty90@gmail.com',
-    subject: `Buddy budget`,
-    text: 'todays update',
-    html: `<h1>new updates</h1>`
-  });
-  await sendMail({
-    from: 'seif.elbosaty3@gmail.com',
-    to: 'seif.elbosaty90@gmail.com',
-    subject: `Buddy budget`,
-    text: 'todays update',
-    html: `<h1>new updates</h1>`
-  });
+
